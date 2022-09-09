69dcfee7c07e6e95f35d413557417c5797ff068a
tharindu <tharindunirmal996@gmail.com>
2022-09-09 16:11:31 +0530

    Modifications in the UI

--- src/components/footer-component/footer.component.jsx
+++ src/components/footer-component/footer.component.jsx
@@ -7,33 +7,7 @@ import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 
 function FooterComponent() {
     return (
-            // <div className='footergogo'>
-        //     <div className='footergogo1'>
-        //         <footer className="text-center text-lg-start" style={{ backgroundColor: "#db6930" }}>
-        //             <div className="container d-flex justify-content-center py-5">
-        //                 <button type="button" className="btn btn-primary btn-lg btn-floating mx-2" style={{ backgroundColor: "#54456b" }}>
-        //                     <i className="fab fa-facebook-f"></i>
-        //                 </button>
-        //                 <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style={{ backgroundColor: "#54456b" }}>
-        //                     <i className="fab fa-youtube"></i>
-        //                 </button>
-        //                 <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style={{ backgroundColor: "#54456b" }}>
-        //                     <i className="fab fa-instagram"></i>
-        //                 </button>
-        //                 <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style={{ backgroundColor: "#54456b" }}>
-        //                     <i className="fab fa-twitter"></i>
-        //                 </button>
-        //             </div>
-
-        //             <div className="text-center text-white p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-        //                 © 2020 Copyright:
-        //                 <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-        //             </div>
-        //         </footer>
-        //     </div>
-        // </div>
-
-
+        
         <div className='footergogo'>
             <div className='footergogo1'>
                 <footer className="bg-dark text-white">
@@ -44,7 +18,7 @@ function FooterComponent() {
                             {/* <i className="fab fa-facebook-f"></i> */}
                             {/* <h7>  */}
                                 {/* <FacebookIcon/>  */}
-                                <DeleteOutlinedIcon />
+                                {/* <DeleteOutlinedIcon /> */}
                                 {/* </h7> */}
                             </a>
                             <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
--- src/components/storeAdmin-components/storeAdminDashboard.jsx
+++ src/components/storeAdmin-components/storeAdminDashboard.jsx
@@ -6,18 +6,22 @@ function storeAdminDashboard() {
     return (
         <>
             <div className="container bkgrnd" style={{ maxWidth: "80%" }}>
-                <div className='container dshbrd' style={{ width: '80%' }}>
-                    <Container>
-                            {/* <div style={{marginTop: '300px'}}> */}
-                        <Row>
-                            <Col> <button type="button" class="btn btn-dark btn-lg"> View Items </button> </Col>
-                            <Col> <button type="button" class="btn btn-dark btn-lg"> Add Item </button> </Col>
-                            <Col> <button type="button" class="btn btn-dark btn-lg"> Generate Reports </button> </Col>
+                <h5 style={{ marginTop: '250px' }}>Welcome, NAME !</h5>
+                <h4> Store Dashboard</h4>
+                <div  className='container dshbrd' style={{ width: '80%' }}>
+                    <Container >
+                        <div >
+                            <Row>
+                                <Col style={{ marginTop: '250px' }}> <button type="button" class="btn btn-dark btn-lg"> View Items </button> </Col>
+                                <Col style={{ marginTop: '300px' }}> <button type="button" class="btn btn-dark btn-lg"> Add Item </button> </Col>
+                                <Col style={{ marginTop: '300px' }}> <button type="button" class="btn btn-dark btn-lg"> Generate Reports </button> </Col>
                             </Row>
-                            {/* </div> */}
+                        </div>
                     </Container>
                 </div>
             </div>
+
+
         </>
     )
 }
